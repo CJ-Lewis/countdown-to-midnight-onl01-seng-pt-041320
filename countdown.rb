1e4e6c5f2c545ecf49e4bfdf7eb25cb6d10a6dd1
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-16 23:39:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ def countdown(number)
 return "HAPPY NEW YEAR!"
 end
 
-# def countdown_with_sleep
-  countdown = 5
-  sleep 5.seconds while countdown > 0
-    puts "#{number} SECOND(S)!"
-    countdown -= 1
-  end
-end
+### def countdown_with_sleep
+#   countdown = 5
+#   sleep 5.seconds while countdown > 0
+#     puts "#{number} SECOND(S)!"
+#     countdown -= 1
+#   end
+# end
